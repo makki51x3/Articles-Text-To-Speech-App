24b0db4aef6cb0e7e426a422f1a214752ed7cce8
51X3 <96151955+makki51x3@users.noreply.github.com>
2022-04-25 16:51:56 -0700

    setting up navigation

---
+++ screens/LoginScreen/Screen.js
@@ -0,0 +1,36 @@
+import { StyleSheet, TextInput, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <TextInput
+        value={this.state.username}
+        onChangeText={(username) => this.setState({ username })}
+        placeholder={'Username'}
+        style={styles.input}
+        />
+        <TextInput
+        value={this.state.password}
+        onChangeText={(password) => this.setState({ password })}
+        placeholder={'Password'}
+        secureTextEntry={true}
+        style={styles.input}
+        />
+        
+        <Button
+        title={'Login'}
+        style={styles.input}
+        onPress={this.onLogin.bind(this)}
+        />
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
