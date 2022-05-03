c5d50cfeb7ecad98c2bfed45f1787b56a0f58cfb
51X3 <96151955+makki51x3@users.noreply.github.com>
2022-05-02 22:03:08 -0700

    SAVE

--- App.js
+++ App.js
@@ -8,22 +8,23 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { store } from './redux/store';
 import * as NavigationBar from "expo-navigation-bar";
 import { useEffect, useState } from 'react';
+import { Platform } from 'react-native';
 
 
 function App() {
   
   const Stack = createNativeStackNavigator();
 
-  // Hide default bottom navigation bar on mobile
-  const [barVisibility, setBarVisibility] = useState();
-
-  NavigationBar.addVisibilityListener(({ visibility }) => {
-    setBarVisibility(visibility);
-  });
-
-  useEffect(() => {
-    NavigationBar.setVisibilityAsync("hidden"); // Hide it
-  }, [barVisibility]);
+  // Hide default bottom navigation bar on android only
+  if (Platform.OS=="android"){
+    const [barVisibility, setBarVisibility] = useState();
+    NavigationBar.addVisibilityListener(({ visibility }) => {
+      setBarVisibility(visibility);
+    });
+    useEffect(() => {
+      NavigationBar.setVisibilityAsync("hidden"); // Hide it
+    }, [barVisibility]);
+  }
 
   return (
     <Provider store={store}>
